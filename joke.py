41e0a758e9b469e672f5d4ed757d64d20a876dd4
Mahkxai <shr.hardik@gmail.com>
2023-10-02 22:57:15 -0400

    Test meme Action on PR

--- joke.py
+++ joke.py
@@ -2,6 +2,7 @@ import json
 import logging
 import re
 from typing import Optional
+import os
 
 import requests
 
@@ -16,6 +17,20 @@ IMAGE_EXTENSIONS_PATTERN = re.compile(r"\.(jpg|jpeg|png)$", re.IGNORECASE)
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
+
+
+def post_comment(pr_url, meme_url, token):
+	"""Comment a meme on every PR"""
+	headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+    }
+	comment = {
+        "body": f"![Meme]({meme_url})"
+    }
+	response = requests.post(pr_url, headers=headers, data=json.dumps(comment))
+	if response.status_code != 201:
+	    logging.error(f"Failed to post comment: {response.text}")
 
 
 def fetch_random_url(api_url: str, user_agent: str) -> Optional[str]:
@@ -54,11 +69,18 @@ def update_readme_with_url(markdown: str) -> bool:
 
 
 def main() -> None:
-    """Update README with a new meme."""
+    # """Update README with a new meme."""
+    """Add meme comment to PR"""
     meme_url = fetch_random_url(REDDIT_API_URL, USER_AGENT)
     if meme_url and IMAGE_EXTENSIONS_PATTERN.search(meme_url):
-        markdown = f"![Funny Meme]({meme_url}?width=100&height=100)"
-        update_readme_with_url(markdown)
+        # markdown = f"![Funny Meme]({meme_url}?width=100&height=100)"
+        # update_readme_with_url(markdown)
+        token = os.environ.get("GITHUB_TOKEN")
+        pr_url = os.environ.get("PR_URL")
+        if token and pr_url:
+            post_comment(pr_url, meme_url, token)
+        else:
+            logging.error("GITHUB_TOKEN or PR_URL environment variables are missing")
 
 
 if __name__ == "__main__":
